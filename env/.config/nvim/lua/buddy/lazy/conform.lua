948704fe5589a91c0c2afe4aa564c0d1341763b9
Badr Choubai <badrchoubai@outlook.com>
2026-04-17 00:14:14 -0600

    update: 2026-04-17

--- env/.config/nvim/lua/buddy/lazy/conform.lua
+++ env/.config/nvim/lua/buddy/lazy/conform.lua
@@ -1,32 +1,31 @@
 return {
-  "stevearc/conform.nvim",
-  config = function()
-    require("conform").setup({
-      format_on_save = function(bufnr)
-        local ft = vim.bo[bufnr].filetype
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            format_on_save = function(bufnr)
+                local ft = vim.bo[bufnr].filetype
 
-        -- Disable autoformat for markdown
-        if ft == "markdown" then
-          return
-        end
+                -- Disable autoformat for markdown
+                if ft == "markdown" then
+                    return
+                end
 
-        return {
-          timeout_ms = 1000,
-          lsp_fallback = true,
-        }
-      end,
+                return {
+                    timeout_ms = 1000,
+                    lsp_fallback = true,
+                }
+            end,
 
-      formatters_by_ft = {
-        go = { "gofmt", "goimports" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        html = { "prettier" },
-      },
-    })
+            formatters_by_ft = {
+                go = { "goimports" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                html = { "prettier" },
+            },
+        })
 
-    vim.keymap.set("n", "<leader>f", function()
-      require("conform").format({ bufnr = 0 })
-    end)
-  end,
+        vim.keymap.set("n", "<leader>f", function()
+            require("conform").format()
+        end)
+    end,
 }
-
--- env/.config/nvim/lua/buddy/lazy/lsp.lua
+++ env/.config/nvim/lua/buddy/lazy/lsp.lua
@@ -1,158 +1,169 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    -- LSP + installer
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        -- LSP + installer
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
 
-    -- Completion
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
+        -- Completion
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
 
-    -- Formatting + UI
-    "stevearc/conform.nvim",
-    "j-hui/fidget.nvim",
-  },
+        -- Formatting + UI
+        "stevearc/conform.nvim",
+        "j-hui/fidget.nvim",
+    },
 
-  config = function()
-    ------------------------------------------------------------------
-    -- LSP capabilities (nvim-cmp aware)
-    ------------------------------------------------------------------
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      vim.lsp.protocol.make_client_capabilities(),
-      require("cmp_nvim_lsp").default_capabilities()
-    )
-
-    ------------------------------------------------------------------
-    -- Diagnostics UI
-    ------------------------------------------------------------------
-    vim.diagnostic.config({
-      float = {
-        focusable = false,
-        border = "rounded",
-        source = "always",
-      },
-    })
-
-    ------------------------------------------------------------------
-    -- Mason
-    ------------------------------------------------------------------
-    require("mason").setup()
-    require("fidget").setup({})
-
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "gopls",
-        "ts_ls",
-        "emmet_ls",
-        "cssls",
-        "html"
-      },
-
-      handlers = {
-        -- Default handler
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
+    config = function()
+        ------------------------------------------------------------------
+        -- LSP capabilities (nvim-cmp aware)
+        ------------------------------------------------------------------
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            vim.lsp.protocol.make_client_capabilities(),
+            require("cmp_nvim_lsp").default_capabilities()
+        )
 
         ------------------------------------------------------------------
-        -- Go
+        -- Diagnostics UI
         ------------------------------------------------------------------
-        ["gopls"] = function()
-          require("lspconfig").gopls.setup({
-            capabilities = capabilities,
-            settings = {
-              gopls = {
-                gofumpt = true,
-                analyses = {
-                  unusedparams = true,
-                  nilness = true,
-                },
-                staticcheck = true,
-              },
+        vim.diagnostic.config({
+            float = {
+                focusable = false,
+                border = "rounded",
+                source = "always",
             },
-          })
-        end,
+        })
 
         ------------------------------------------------------------------
-        -- JavaScript / TypeScript
+        -- Mason
         ------------------------------------------------------------------
-        ["tsserver"] = function()
-          require("lspconfig").tsserver.setup({
-            capabilities = capabilities,
-            settings = {
-              completions = {
-                completeFunctionCalls = true,
-              },
+        require("mason").setup()
+        require("fidget").setup({})
+
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "gopls",
+                "ts_ls",
+                "emmet_ls",
+                "cssls",
+                "html"
             },
-          })
-        end,
-        ["html"] = function()
-          require("lspconfig").html.setup({
-            capabilities = capabilities,
-            settings = {
-              html = {
-                format = { wrapLineLength = 120 },
-                hover = { documentation = true, references = true },
-              },
+
+            handlers = {
+                -- Default handler
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+
+                ------------------------------------------------------------------
+                -- Go
+                ------------------------------------------------------------------
+                ["gopls"] = function()
+                    require("lspconfig").gopls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            gopls = {
+                                gofumpt = true,
+                                analyses = {
+                                    unusedparams = true,
+                                    nilness = true,
+                                },
+                                staticcheck = true,
+                            },
+                        },
+                    })
+                end,
+
+                vim.filetype.add({
+                    filename = {
+                        ["docker-compose.yml"] = "yaml.docker-compose",
+                        ["docker-compose.yaml"] = "yaml.docker-compose",
+                        ["compose.yml"] = "yaml.docker-compose",
+                        ["compose.yaml"] = "yaml.docker-compose",
+                    },
+                    pattern = {
+                        [".*docker%-compose.*%.ya?ml"] = "yaml.docker-compose",
+                    },
+                }),
+
+                ------------------------------------------------------------------
+                -- JavaScript / TypeScript
+                ------------------------------------------------------------------
+                ["tsserver"] = function()
+                    require("lspconfig").tsserver.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            completions = {
+                                completeFunctionCalls = true,
+                            },
+                        },
+                    })
+                end,
+                ["html"] = function()
+                    require("lspconfig").html.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            html = {
+                                format = { wrapLineLength = 120 },
+                                hover = { documentation = true, references = true },
+                            },
+                        },
+                    })
+                end,
+                ["cssls"] = function()
+                    require("lspconfig").cssls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            css = { validate = true },
+                            scss = { validate = true },
+                            less = { validate = true },
+                        },
+                    })
+                end,
+                ["emmet_ls"] = function()
+                    require("lspconfig").emmet_ls.setup({
+                        capabilities = capabilities,
+                        filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact" },
+                    })
+                end,
             },
-          })
-        end,
-        ["cssls"] = function()
-          require("lspconfig").cssls.setup({
-            capabilities = capabilities,
-            settings = {
-              css = { validate = true },
-              scss = { validate = true },
-              less = { validate = true },
+        })
+
+        ------------------------------------------------------------------
+        -- nvim-cmp
+        ------------------------------------------------------------------
+        local cmp = require("cmp")
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
             },
-          })
-        end,
-        ["emmet_ls"] = function()
-          require("lspconfig").emmet_ls.setup({
-            capabilities = capabilities,
-            filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact" },
-          })
-        end,
-      },
-    })
 
-    ------------------------------------------------------------------
-    -- nvim-cmp
-    ------------------------------------------------------------------
-    local cmp = require("cmp")
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            mapping = cmp.mapping.preset.insert({
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-      }, {
-        { name = "buffer" },
-      }),
-    })
-  end,
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "path" },
+            }, {
+                { name = "buffer" },
+            }),
+        })
+    end,
 }
-
